bd7ec7caa0576f08c00bbbf993a9204f93be16e3
Paul Cantrell <cantrell@pobox.com>
2016-09-03 09:58:48 -0500

    Nullability annotations for delay/go

--- Nocilla/DSL/LSStubResponseDSL.h
+++ Nocilla/DSL/LSStubResponseDSL.h
@@ -8,7 +8,7 @@
 typedef LSStubResponseDSL * _Nonnull (^ResponseWithBodyMethod)(_Nullable id<LSHTTPBody>);
 typedef LSStubResponseDSL * _Nonnull (^ResponseWithHeaderMethod)(NSString * _Nonnull, NSString * _Nullable);
 typedef LSStubResponseDSL * _Nonnull (^ResponseWithHeadersMethod)(NSDictionary * _Nonnull);
-typedef LSStubResponseDSL *(^ResponseVoidMethod)();
+typedef LSStubResponseDSL * _Nonnull (^ResponseVoidMethod)();
 
 @interface LSStubResponseDSL : NSObject
 - (id _Nonnull)initWithResponse:(LSStubResponse * _Nonnull)response;
@@ -17,7 +17,7 @@ typedef LSStubResponseDSL *(^ResponseVoidMethod)();
 @property (nonatomic, strong, readonly) _Nonnull ResponseWithHeadersMethod withHeaders;
 @property (nonatomic, strong, readonly) _Nonnull ResponseWithBodyMethod withBody;
 
-@property (nonatomic, strong, readonly) ResponseVoidMethod delay;
-@property (nonatomic, strong, readonly) ResponseVoidMethod go;
+@property (nonatomic, strong, readonly) _Nonnull ResponseVoidMethod delay;
+@property (nonatomic, strong, readonly) _Nonnull ResponseVoidMethod go;
 
 @end
